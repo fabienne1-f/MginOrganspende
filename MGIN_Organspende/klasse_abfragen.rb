2c1c7ebd25739f60f0f6c4e54a63f0e65174d3a1
Fabienne <sch19029@spengergasse.at>
2021-06-17 11:41:00 +0200

    Hinzüfen weiterer Methoden

--- MGIN_Organspende/klasse_abfragen.rb
+++ MGIN_Organspende/klasse_abfragen.rb
@@ -3,10 +3,29 @@
 class KlasseAbfragen
   def abfrage_patient_blutgruppe(blutgruppePatient)
     if blutgruppePatient == "A+" or blutgruppePatient == "A-" or blutgruppePatient == "B+" or blutgruppePatient == "B-" or blutgruppePatient == "AB+" or blutgruppePatient == "AB-" or blutgruppePatient == "0+" or blutgruppePatient == "0-"
-      return true
+        return true
 
     else
       puts"Keine gültige Blutgruppe"
     end
   end
-end
+
+  def abfrage_spender_blutgruppe(blutgruppeSpender)
+    if blutgruppeSpender == "A+" or blutgruppeSpender == "A-" or blutgruppeSpender == "B+" or blutgruppeSpender == "B-" or blutgruppeSpender == "AB+" or blutgruppeSpender == "AB-" or blutgruppeSpender == "0+" or blutgruppeSpender == "0-"
+      return true
+
+    else
+      puts"Keine gültige Blutgruppe"
+
+    end
+
+    def kompatibilitaet_blutgruppe(blutgruppePatient, blutgruppeSpender)
+      if blutgruppePatient == blutgruppeSpender or blutgruppePatient == "0+" or blutgruppeSpender =="0+" or  blutgruppeSpender or blutgruppePatient == "A+" or blutgruppeSpender =="A-" or  blutgruppeSpender or blutgruppePatient == "A+" or blutgruppeSpender =="B+"
+        return true
+
+      else
+        puts "Leider nicht kompatibel"
+      end
+  end
+  end
+  end
